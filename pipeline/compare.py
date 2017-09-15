ae1e66c9c967a30dbecb41f65864889a07313e36
sgongar <s.gongoragarcia@gmail.com>
2017-09-15 20:18:28 +0200

    compare.py updated to version 0.5

--- pipeline/compare.py
+++ pipeline/compare.py
@@ -11,6 +11,8 @@ Versions:
         with all the CCDs.
         This version is not correct as we don't have the correct header data.
 * 0.3 - Third version.
+* 0.4 - 
+* 0.5 - Creates regions for input and output catalogs.
 
 In order to improve legibilidad algunas abreviaturas han sido creadas
 * c = catalog
@@ -40,7 +42,7 @@ from misc import extract_settings, get_fits, check_distance
 __author__ = "Samuel Gongora-Garcia"
 __copyright__ = "Copyright 2017"
 __credits__ = ["Samuel Gongora-Garcia"]
-__version__ = "0.3"
+__version__ = "0.4"
 __maintainer__ = "Samuel Gongora-Garcia"
 __email__ = "sgongora@cab.inta-csic.es"
 __status__ = "Development"
@@ -55,37 +57,53 @@ class Compare:
         """
         prfs_d = extract_settings()
 
-        # Perfoms an analysis
-        if not self.perform_analysis(prfs_d):
-            raise Exception
+        # Perfoms an analysis TODO Create a custom Exception!
+        self.perform_analysis(prfs_d)
 
     def perform_analysis(self, prfs_d):
-        """
+        """ This method obtains the catalogues under study and launches
+        different threads for each pair of catalogues.
 
-        For now location is hard-coded. In posterior versions of this script
-        this should be fixed in order to allow a more complete analysis along
-        different sextractor and scamp configurations.
+        TODO For now location is hard-coded. In posterior versions of this
+        script this should be fixed in order to allow a more complete analysis
+        along different sextractor and scamp configurations.
 
         @param prfs_d: a regular preferences dictionary.
 
         @return True: if everything is alright.
         """
         # Hardcoded references.
-        folder_sex = '2_10_10_0.1_4'
-        folder_scmp = '150_1.2_5_0.033'
+        folder_sex = '20_10_10_0.1_4'
+        # folder_scmp = '150_1.2_5_0.033'
 
         # Load scamp's catalogs.
-        scamp_cats_dir = '{}/{}/{}'.format(prfs_d['catalogs_dir'], folder_sex,
-                                           folder_scmp)
-        scamp_cats = self.load_scamp_catalogs(prfs_d, scamp_cats_dir)
+        # scamp_cats_dir = '{}/{}/{}'.format(prfs_d['catalogs_dir'],
+        #                                    folder_sex, folder_scmp)
+        # scamp_cats = self.load_scamp_catalogs(prfs_d, scamp_cats_dir)
 
         # Load sextractor's catalogs. Option to filter
+        # sex_cats_dir = '{}/{}'.format(prfs_d['fits_dir'], folder_sex)
+        # sex_cats = self.load_sex_catalogs(prfs_d, sex_cats_dir, unique=True)
+
+        # Load sextractor's reference catalogs.
         sex_cats_dir = '{}/{}'.format(prfs_d['fits_dir'], folder_sex)
-        sex_cats = self.load_sex_catalogs(prfs_d, sex_cats_dir, unique=True)
+        sex_cats_i = self.load_sex_catalogs(prfs_d, sex_cats_dir,
+                                            unique=True, filter=False)
+
+        # Load sextractor's full populated catalogs.
+        fits_dir = '/pcdisk/kool5/sgongora/Documents/luca_data/v14/CCDs'
+        sex_cats_dir = '{}/{}'.format(fits_dir, folder_sex)
+        sex_cats_o = self.load_sex_catalogs(prfs_d, sex_cats_dir,
+                                            unique=True, filter=False)
 
         # Unique flag allow us to get a list of references populated only by
         # images of first dither.
-        fits_files = get_fits(unique=False)
+        fits_files = get_fits(unique=True)
+
+        # In order to improve script reuse, references to catalogues
+        # will be dynamic.
+        input_cats = [sex_cats_i, ['NUMBER', 'ALPHA_J2000', 'DELTA_J2000']]
+        output_cats = [sex_cats_o, ['NUMBER', 'ALPHA_J2000', 'DELTA_J2000']]
 
         # TODO '17' value is harcoded to this (24 cores/48 threads) CPU.
         # Should be change to a relative value related to microprocessor used.
@@ -100,8 +118,8 @@ class Compare:
                                                    fits_files[idx_p][:-5])
 
                     compare_p = Process(target=self.perform_analysis_thread,
-                                        args=(fits_n, idx_p, prfs_d,
-                                              scamp_cats, sex_cats,))
+                                        args=(fits_n, prfs_d,
+                                              input_cats, output_cats,))
                     compare_j.append(compare_p)
                     compare_p.start()
 
@@ -110,18 +128,19 @@ class Compare:
                 active_compare = list([job.is_alive() for job in compare_j])
                 pass
 
-        self.merge_stats(fits_files, prfs_d)
+        # Creates a file with all the statistics obtained.
+        if not self.merge_stats(fits_files, prfs_d):
+            raise Exception  # TODO Creates a custom Exception.
 
         return True
 
-    def perform_analysis_thread(self, fits_n, idx, prfs_d,
-                                scamp_cats, sex_cats):
+    def perform_analysis_thread(self, fits_n, prfs_d, input_cats, output_cats):
         """
 
         @param fits_n:
-        @param idx:
-        @param prfs_d:
-        @param cats:
+        @param prfs_d: A dictionary with all script preferences.
+        @param input_cats:
+        @param output_cats:
         """
         # Set-up indexes for this particular case
         idx_detected = 0
@@ -130,21 +149,26 @@ class Compare:
         # Set-up dictionaries for this particular case
         stats_d = {}
         sources_d = {}
-        stats_d, sources_d = self.populate_dict(stats_d, sources_d)
+        i_comp_d = {}
+        o_comp_d = {}
+        (stats_d, sources_d,
+         i_comp_d, o_comp_d) = self.populate_dict(stats_d, sources_d,
+                                                  i_comp_d, o_comp_d)
+
         ccd_borders_d = self.define_ccd_borders(prfs_d)
 
         stats_d['CCD'].append(fits_n[-12:-4])
         stats_d['dither'].append(fits_n[-5:-4])
 
-        fits_table = sex_cats[fits_n[-12:-4]]
+        input_table = input_cats[0][fits_n[-12:-4]]
         # Removing zero catalog references
         # fits_table = fits_table.loc[~fits_table['CATALOG_NUMBER'].isin([0])]
-        stats_d['total'].append(len(fits_table['NUMBER'].tolist()))
+        stats_d['total'].append(len(input_table['NUMBER'].tolist()))
 
         # All variables starting by 'cat' are referred to scamp output catalogs
         # All variables starting by 'fits' are referred to sextractor catalogs
-        fits_sources = fits_table['NUMBER'].tolist()
-        for fits_source in fits_sources:
+        input_sources = input_table['NUMBER'].tolist()
+        for input_source in input_sources:
             # Reset variables associated to each input source
             distances_cache = []
             i_alpha_cache = []
@@ -153,40 +177,40 @@ class Compare:
             o_delta_cache = []
 
             # Gets input data associated to selectes source
-            fits_t = fits_table[fits_table['NUMBER'].isin([fits_source])]
-            fits_ra = float(fits_t['ALPHA_J2000'])
-            fits_dec = float(fits_t['DELTA_J2000'])
+            input_t = input_table[input_table['NUMBER'].isin([input_source])]
+            input_ra = float(input_t['ALPHA_J2000'])
+            input_dec = float(input_t['DELTA_J2000'])
 
-            ccd_ = self.look_for_ccd(prfs_d, fits_ra, fits_dec, ccd_borders_d)
-
+            ccd_ = self.look_for_ccd(prfs_d, input_ra, input_dec,
+                                     ccd_borders_d)
             if ccd_ is not None:
-                cat_table = scamp_cats[ccd_]
+                output_table = output_cats[0][ccd_]
 
                 # To improve comparation speed output catalog is reduced
-                margins = [[fits_ra, 'ALPHA_J2000'],
-                           [fits_dec, 'DELTA_J2000']]
+                margins = [[input_ra, 'ALPHA_J2000'],
+                           [input_dec, 'DELTA_J2000']]
                 margin = 2 * prfs_d['tolerance']
-                cat_table_cut = cut_catalog(cat_table, margins, margin)
+                output_table_cut = cut_catalog(output_table, margins, margin)
 
                 # Takes a look over output sources
                 # Each source has only one reference
-                for cat_source in cat_table_cut['SOURCE_NUMBER'].tolist():
-                    mask = cat_table_cut['SOURCE_NUMBER'].isin([cat_source])
-                    cat_t = cat_table_cut[mask]
-                    cat_ra = float(cat_t['ALPHA_J2000'])
-                    cat_dec = float(cat_t['DELTA_J2000'])
-                    # Compare cat_ra, cat_dec against fits_ra, fits_dec
-                    close, distance = check_distance(fits_ra, cat_ra,
-                                                     fits_dec, cat_dec,
+                for output_source in output_table_cut['NUMBER'].tolist():
+                    mask = output_table_cut['NUMBER'].isin([output_source])
+                    output_t = output_table_cut[mask]
+                    output_ra = float(output_t['ALPHA_J2000'])
+                    output_dec = float(output_t['DELTA_J2000'])
+                    # Compare output_ra, output_dec against input_ra, input_dec
+                    close, distance = check_distance(input_ra, output_ra,
+                                                     input_dec, output_dec,
                                                      prfs_d['tolerance'])
                     if close:
                         close_flag = True
                         distance = distance * 3600  # From degrees to seconds
                         distances_cache.append(distance)
-                        i_alpha_cache.append(cat_ra)
-                        i_delta_cache.append(cat_dec)
-                        o_alpha_cache.append(fits_ra)
-                        o_delta_cache.append(fits_dec)
+                        i_alpha_cache.append(input_ra)
+                        i_delta_cache.append(input_dec)
+                        o_alpha_cache.append(output_ra)
+                        o_delta_cache.append(output_dec)
 
                 if len(distances_cache) > 1 and close_flag is True:
                     # These values will be the same whatever the output is.
@@ -227,20 +251,18 @@ class Compare:
                     sources_d['o_DELTA_J2000'].append(o_delta)
                     idx_detected += 1
                 elif len(distances_cache) == 0:
-                    """
-                    distance_ = ''
-                    sources_d['distance'].append(distance_)
-                    sources_d['duplicated'].append('')
-                    i_alpha = i_alpha_cache[idx_cache]
-                    sources_d['i_ALPHA_J2000'].append(i_alpha)
-                    i_delta = i_delta_cache[idx_cache]
-                    sources_d['i_DELTA_J2000'].append(i_delta)
-                    o_alpha = ''
-                    sources_d['o_ALPHA_J2000'].append(o_alpha)
-                    o_delta = ''
-                    sources_d['o_DELTA_J2000'].append(o_delta)
-                    """
                     idx_lost += 1
+
+                if distance_ > 0.1:
+                    idx_cache = distances_cache.index(min(distances_cache))
+                    i_alpha = i_alpha_cache[idx_cache]
+                    i_comp_d['i_ALPHA_J2000'].append(i_alpha)
+                    i_delta = i_delta_cache[idx_cache]
+                    i_comp_d['i_DELTA_J2000'].append(i_delta)
+                    o_alpha = o_alpha_cache[idx_cache]
+                    o_comp_d['o_ALPHA_J2000'].append(o_alpha)
+                    o_delta = o_delta_cache[idx_cache]
+                    o_comp_d['o_DELTA_J2000'].append(o_delta)
 
             else:
                 idx_lost += 1
@@ -264,8 +286,22 @@ class Compare:
                                    'i_ALPHA_J2000', 'i_DELTA_J2000',
                                    'o_ALPHA_J2000', 'o_DELTA_J2000'])
 
+        i_comp_df = DataFrame(i_comp_d)
+        i_comp_df.to_csv('{}/i_regions_{}.reg'.format(prfs_d['tmp_out'],
+                                                      fits_n[-12:-4]),
+                         columns=['i_ALPHA_J2000', 'i_DELTA_J2000'],
+                         index=False, header=False, sep=" ")
+
+        o_comp_df = DataFrame(o_comp_d)
+        o_comp_df.to_csv('{}/o_regions_{}.reg'.format(prfs_d['tmp_out'],
+                                                      fits_n[-12:-4]),
+                         columns=['o_ALPHA_J2000', 'o_DELTA_J2000'],
+                         index=False, header=False, sep=" ")
+
     def define_ccd_borders(self, prfs_d):
         """
+
+        @param prfs_d: A dictionary with all script preferences.
 
         @return ccd_borders_d:
         """
@@ -287,6 +323,12 @@ class Compare:
     def look_for_ccd(self, prfs_d, ra, dec, ccd_borders_d):
         """
 
+        @param prfs_d: A dictionary with all script preferences.
+        @param ra:
+        @param dec:
+        @param ccd_borders_d:
+
+        @return key_:
         """
         for key_ in ccd_borders_d.keys():
             ccd_borders = ccd_borders_d[key_]
@@ -299,7 +341,7 @@ class Compare:
     def load_scamp_catalogs(self, prfs_d, cats_dir):
         """
 
-        @param prfs_d:
+        @param prfs_d: A dictionary with all script preferences.
         @param cats_dir:
 
         @retun cat_d;
@@ -323,15 +365,44 @@ class Compare:
 
         return cat_d
 
-    def load_sex_catalogs(self, prfs_d, sex_cats_dir, unique):
+    def load_sex_catalogs(self, prfs_d, sex_cats_dir, unique, filter):
         """
 
-        @param prfs_d:
-        @param unique: a flag for
+        @param prfs_d: A dictionary with all script preferences.
+        @param sex_cats_dir: A string with the catalog location.
+        @param unique: If active, only images from the first dither
+                       will be used.
+        @param filter: If active catalogs will be shrink to dither one limits.
 
-        @return cat_d:
+        @return cat_d: A dictionary populated by sextractor's catalogs.
         """
         cat_d = {}  # A dictionary for catalogs.
+
+        if filter:
+            margins_d = self.get_catalog_limits(prfs_d, unique)
+
+        fits_files = get_fits(unique=True)
+        for fits_ in fits_files:
+            sex_c = '{}/{}.cat'.format(sex_cats_dir, fits_[:-5])
+
+            cat_file = fits.open(sex_c)
+            cat_data = Table(cat_file[2].data)
+            cat_table = cat_data.to_pandas()
+
+            if filter:
+                cat_d[fits_[-13:-5]] = shrink_catalog(cat_table, margins_d)
+            else:
+                cat_d[fits_[-13:-5]] = cat_table
+
+        return cat_d
+
+    def get_catalog_limits(self, prfs_d, unique):
+        """
+        @param prfs_d:
+        @param unique:
+
+        @return margins_d:
+        """
         limits_d = {}  # A dictionay for limits.
 
         # Populates dictionary with lists
@@ -360,19 +431,9 @@ class Compare:
         margins_d = {'above_ra': above_ra, 'below_ra': below_ra,
                      'above_dec': above_dec, 'below_dec': below_dec}
 
-        fits_files = get_fits(unique=False)
-        for fits_ in fits_files:
-            sex_c = '{}/{}.cat'.format(sex_cats_dir, fits_[:-5])
+        return margins_d
 
-            cat_file = fits.open(sex_c)
-            cat_data = Table(cat_file[2].data)
-            cat_table = cat_data.to_pandas()
-
-            cat_d[fits_[-13:-5]] = shrink_catalog(cat_table, margins_d)
-
-        return cat_d
-
-    def populate_dict(self, stats_d, sources_d):
+    def populate_dict(self, stats_d, sources_d, i_comp_d, o_comp_d):
         """ populates dictionaries with selected keys
 
         @param stats_d:
@@ -388,13 +449,18 @@ class Compare:
                      'i_ALPHA_J2000': [], 'i_DELTA_J2000': [],
                      'o_ALPHA_J2000': [], 'o_DELTA_J2000': []}
 
-        return stats_d, sources_d
+        i_comp_d = {'i_ALPHA_J2000': [], 'i_DELTA_J2000': []}
+        o_comp_d = {'o_ALPHA_J2000': [], 'o_DELTA_J2000': []}
+
+        return stats_d, sources_d, i_comp_d, o_comp_d
 
     def merge_stats(self, fits_files, prfs_d):
         """
 
         @param fits_files:
         @param prfs_d:
+
+        @return True: if everything goes alright.
         """
         cat_list = []
 
@@ -409,6 +475,8 @@ class Compare:
 
         stats_cat = concat(cat_list, axis=0)
         stats_cat.to_csv('stats.csv')
+
+        return True
 
 
 if __name__ == '__main__':
