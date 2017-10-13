f6c89b85592e7fc979cdf3a092a33141d1fb0eed
sgongar <s.gongoragarcia@gmail.com>
2017-10-13 13:07:38 +0200

    simple test

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""Python script for time measurements
+
+
+Todo:
+    * Improve log messages
+
+"""
+
+from unittest import TestCase, main
+
+
+__author__ = "Samuel Gongora-Garcia"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Samuel Gongora-Garcia"]
+"""
+__license__ = "GPL"
+"""
+__version__ = "0.1"
+__maintainer__ = "Samuel Gongora-Garcia"
+__email__ = "sgongora@cab.inta-csic.es"
+__status__ = "Development"
+
 
 def main():
 
