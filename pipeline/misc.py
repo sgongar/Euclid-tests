a418043b6ab115c574cd13da289de5bd493d2ebb
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-04-18 17:53:16 +0200

    test for filter process for SC3 images

--- pipeline/misc.py
+++ pipeline/misc.py
@@ -614,12 +614,12 @@ def pm_compute(logger, merged_db, full_db):
 
     for idx_merged, source in enumerate(merged_db['SOURCE_NUMBER']):
         part_idx = 0
-        first_idx += 1
         # print('{} - {}'.format(idx_merged, len(merged_db['SOURCE_NUMBER'])))
         full_p_db = full_db[full_db['SOURCE_NUMBER'].isin([source])]
 
         for idx in full_p_db['SOURCE_NUMBER']:
             part_idx += 1
+            first_idx += 1
             pmalpha_l.append(pmalpha.iloc[idx_merged])
             pmdelta_l.append(pmdelta.iloc[idx_merged])
             pmealpha_l.append(pmealpha.iloc[idx_merged])
@@ -628,6 +628,7 @@ def pm_compute(logger, merged_db, full_db):
             pme_l.append(pme.iloc[idx_merged])
 
         print('part {}'.format(part_idx))
+        print('total {}'.format(first_idx))
 
     print('total {}'.format(first_idx))
 
