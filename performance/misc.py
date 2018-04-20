15e5b98195689f3cec6a064776d0985c9c96e301
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-04-20 15:12:18 +0200

    test to stars/galaxies a/b_image size in sc3 simulated images

---
+++ performance/misc.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Python script for time measurements
+
+
+Todo:
+    * Improve log messages
+    * Improve usability
+"""
+
+from multiprocessing import cpu_count
+from ConfigParser import ConfigParser
+from platform import platform
+
+from errors import BadSettings
+
+__author__ = "Samuel Góngora García"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Samuel Góngora García"]
+__version__ = "0.1"
+__maintainer__ = "Samuel Góngora García"
+__email__ = "sgongora@cab.inta-csic.es"
+__status__ = "Development"
+
+
+def get_os():
+    """ a function that gets the current operative system
+    for now works in Debian, Fedora and Ubuntu shell (Microsoft version)
+
+    @return os_system: a string which contains the operative system name
+    """
+    if 'fedora-19' in platform():
+        os_system = 'cab'
+    elif 'centos' in platform():
+        os_system = 'centos'
+    else:
+        raise Exception
+
+    return os_system
+
+
+def conf_map(config_, section):
+    """
+
+    @param config_:
+    @param section:
+
+    @return dict1:
+    """
+    dict1 = {}
+    options = config_.options(section)
+    for option in options:
+        try:
+            dict1[option] = config_.get(section, option)
+            if dict1[option] == -1:
+                print('skip: {}'.format(option))
+        except KeyError:
+            print('exception on {}'.format(option))
+            dict1[option] = None
+    return dict1
+
+
+def extract_settings_sc3():
+    """ creates a dictionary with all the configuration parameters
+        at this moment configuration file location is fixed at main directory
+
+    @return prfs_d: a dictionary which contains all valuable data
+    """
+    cf = ConfigParser()
+    cf.read(".settings_SC3.ini")
+
+    prfs_d = {}
+    os_version = get_os()
+
+    print(cf)
+    print('os {}'.format(os_version))
+
+    if os_version == 'centos':
+        prfs_d['version'] = conf_map(cf, "Version")['centos_version']
+    elif os_version == 'cab':
+        prfs_d['version'] = conf_map(cf, "Version")['cab_version']
+    else:
+        raise BadSettings('Operative system not chosen')
+
+    if os_version == 'centos':
+        prfs_d['home'] = conf_map(cf, "HomeDirs")['centos_home']
+    elif os_version == 'cab':
+        prfs_d['home'] = conf_map(cf, "HomeDirs")['cab_home']
+    else:
+        raise BadSettings('Operative system not chosen')
+
+    prfs_d['fits_dir'] = conf_map(cf, "ImagesDirs")['fits_dir']
+    prfs_d['fits_dir'] = '{}{}'.format(prfs_d['version'], prfs_d['fits_dir'])
+
+    # todo - comment!
+    prfs_d['output_cats'] = conf_map(cf, "CatsDirs")['output_cats']
+    prfs_d['output_cats'] = prfs_d['version'] + prfs_d['output_cats']
+    # todo - comment!
+    prfs_d['references'] = conf_map(cf, "CatsDirs")['references']
+    prfs_d['references'] = prfs_d['version'] + prfs_d['references']
+    # todo - comment!
+    prfs_d['filtered'] = conf_map(cf, "CatsDirs")['filtered']
+    prfs_d['filtered'] = prfs_d['version'] + prfs_d['filtered']
+
+    outputdirs_list = ['conf_scamp', 'conf_sex', 'params_sex', 'neural_sex',
+                       'params_cat', 'logger_config']
+    for conf_ in outputdirs_list:
+        prfs_d[conf_] = conf_map(cf, "ConfigDirs")[conf_]
+        prfs_d[conf_] = prfs_d['home'] + prfs_d[conf_]
+
+    prfs_d['detections'] = int(conf_map(cf, "Misc")['detections'])
+    prfs_d['pm_low'] = float(conf_map(cf, "Misc")['pm_low'])
+    prfs_d['pm_up'] = float(conf_map(cf, "Misc")['pm_up'])
+    prfs_d['pm_sn'] = float(conf_map(cf, "Misc")['pm_sn'])
+    pms = conf_map(cf, "Misc")['pms']
+    pms = pms.replace(",", " ")
+    prfs_d['pms'] = [float(x) for x in pms.split()]
+    prfs_d['r_fit'] = conf_map(cf, "Misc")['r_fit']
+    prfs_d['cores_number'] = conf_map(cf, "Misc")['cores_number']
+    if prfs_d['cores_number'] == '0':
+        prfs_d['cores_number'] = int(str(cpu_count()))
+        # TODO should leave free at least 20% of processors
+    else:
+        prfs_d['cores_number'] = int(prfs_d['cores_number'])
+    prfs_d['tolerance'] = float(conf_map(cf, "Misc")['tolerance'])
+
+    return prfs_d
