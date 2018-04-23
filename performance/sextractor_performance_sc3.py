e440db01bc0c82338a008a86947f20f11b484bc3
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-04-23 13:32:21 +0200

    update for a/b image extraction

--- performance/sextractor_performance_sc3.py
+++ performance/sextractor_performance_sc3.py
@@ -342,9 +342,9 @@ def ab_size(input_catalog, full_cat, pdf):
             ax_1.plot(mag_iso_l, a_image_l, label='mean value')
             ax_1.plot(mag_iso_l, b_image_l, label='std value')
 
-        pdf.savefig()  # saves figure
-        plt.clf()  # clear current figure
-        plt.close(fig)  # removes figure
+            pdf.savefig()  # saves figure
+            plt.clf()  # clear current figure
+            plt.close(fig)  # removes figure
 
 
 if __name__ == "__main__":
