3e6a1360f4a300f0900f3a108c49cb16b431f5b0
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-04-24 13:58:51 +0200

    A helpful message will be display if no correct option is choosen

--- performance/sextractor_performance_sc3.py
+++ performance/sextractor_performance_sc3.py
@@ -346,7 +346,12 @@ class ABPerformance:
 if __name__ == "__main__":
     input_catalog_name = '1523634221737L.csv'
 
-    if argv[1] == '-extracted':
-        extracted = ExtractionPerformance(input_catalog_name)
-    elif argv[1] == '-ab_size':
-        ab_size = ABPerformance(input_catalog_name)
+    try:
+        if argv[1] == '-extracted':
+            extracted = ExtractionPerformance(input_catalog_name)
+        elif argv[1] == '-ab_size':
+            ab_size = ABPerformance(input_catalog_name)
+    except IndexError:
+        print('Select a valid option:')
+        print(' - extracted')
+        print(' - ab_size')
