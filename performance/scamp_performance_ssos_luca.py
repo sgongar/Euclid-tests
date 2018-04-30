b585ed66e820251c0aedd82bf5a0aad934e717c2
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-04-30 18:33:48 +0200

    test for a/b size extraction and plotting for luca's images

--- performance/scamp_performance_ssos_luca.py
+++ performance/scamp_performance_ssos_luca.py
@@ -214,18 +214,18 @@ class ScampPerformanceSSOs:
         self.save = True
         self.tmp_d = redo_tmp_d()
 
+        i_pm_l = []
+        n_se_l = []
+        n_false_l = []
+        n_true_l = []
+        n_meas_l = []
+        f_pur_l = []
+        f_dr_l = []
+        f_com_l = []
+
         for mag_ in self.prfs_d['mags']:
             self.mag = mag_
             stats_d = self.check_pm_distribution()
-
-            i_pm_l = []
-            n_se_l = []
-            n_false_l = []
-            n_true_l = []
-            n_meas_l = []
-            f_pur_l = []
-            f_dr_l = []
-            f_com_l = []
 
             for pm_ in self.prfs_d['pms']:
                 print(pm_)
@@ -238,18 +238,18 @@ class ScampPerformanceSSOs:
                 f_dr_l.append(stats_d[mag_][pm_]['f_pur'])
                 f_com_l.append(stats_d[mag_][pm_]['f_com'])
 
-            i_pm_s = Series(i_pm_l, name='i_pm')
-            n_se_s = Series(n_se_l, name='n_se')
-            n_false_s = Series(n_false_l, name='n_false')
-            n_true_s = Series(n_true_l, name='n_true')
-            n_meas_s = Series(n_meas_l, name='n_meas')
-            f_dr_s = Series(f_dr_l, name='f_dr')
-            f_pur_s = Series(f_pur_l, name='f_pur')
-            f_com_s = Series(f_com_l, name='f_com')
+        i_pm_s = Series(i_pm_l, name='i_pm')
+        n_se_s = Series(n_se_l, name='n_se')
+        n_false_s = Series(n_false_l, name='n_false')
+        n_true_s = Series(n_true_l, name='n_true')
+        n_meas_s = Series(n_meas_l, name='n_meas')
+        f_dr_s = Series(f_dr_l, name='f_dr')
+        f_pur_s = Series(f_pur_l, name='f_pur')
+        f_com_s = Series(f_com_l, name='f_com')
 
-            df = concat([i_pm_s, n_se_s, n_false_s, n_true_s, n_meas_s,
-                         f_dr_s, f_pur_s, f_com_s], axis=1)
-            df.to_csv('{}.csv'.format(mag_))
+        df = concat([i_pm_s, n_se_s, n_false_s, n_true_s, n_meas_s,
+                     f_dr_s, f_pur_s, f_com_s], axis=1)
+        df.to_csv('total.csv')
 
         self.plot()
 
