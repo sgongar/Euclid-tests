ba00a68b9a1139cb24912269a7fb6ec3cc521694
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-07-18 22:18:51 +0200

    new class filter value

--- pipeline/scamp_filter_elvis.py
+++ pipeline/scamp_filter_elvis.py
@@ -533,7 +533,7 @@ class ScampFilterELViS:  # TODO Split scamp_filter method into single methods
             pm = float(o_df['PM'])
             class_star = float(o_df['MEAN_CLASS_STAR'])
 
-            if pm < 0.6 and class_star < 0.90:
+            if pm < 0.5 and class_star < 0.90:
                 rejected.append(source_)
             else:
                 accepted.append(source_)
