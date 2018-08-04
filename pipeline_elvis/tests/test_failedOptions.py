d5038c7292387a8139577579b84d6868568613bb
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-08-04 23:17:15 +0200

    update

--- pipeline_elvis/tests/test_failedOptions.py
+++ pipeline_elvis/tests/test_failedOptions.py
@@ -76,9 +76,7 @@ class TestCheckUnsuccessfulOptions(TestCase):
 
         sys.argv.append('-full')
 
-        print(check_elvis.Check())
-
-        # return self.assertRaises(FullPipelineFailed, check_elvis.Check)
+        return self.assertRaises(FullPipelineFailed, check_elvis.Check())
 
     # @patch('misc.extract_settings_elvis')
     # @patch('misc.setting_logger')
